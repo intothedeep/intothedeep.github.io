1483ad64457d6d5d1e53b136230a0f2c43b8f2f3
Taek Lim <tlim41@gatech.edu>
2025-03-24 14:22:17 -0500

    test: profile active & deploy

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,47 +1,43 @@
-// import React, { Suspense } from 'react';
-// import clsx from 'clsx';
+import React, { Suspense } from 'react';
+import clsx from 'clsx';
 
-// import FadeIn from '@/components/Profile/FadeIn';
+import FadeIn from '@/components/Profile/FadeIn';
+import Loading from '@/app/loading';
+
+// Lazy load components
+const Main = React.lazy(() => import('@/components/Profile/About'));
+const Expertise = React.lazy(() => import('@/components/Profile/Expertise'));
+const Timeline = React.lazy(() => import('@/components/Profile/Timeline'));
+const Project = React.lazy(() => import('@/components/Profile/Project'));
+
+// 클라이언트 컴포넌트 동적 로딩
+// import dynamic from 'next/dynamic';
+// const Timeline = dynamic(() => import('./Timeline'), { ssr: false });
+
+import '@/components/Profile/index.scss';
 // import Loading from '@/app/loading';
 
-// // Lazy load components
-// const Main = React.lazy(() => import('@/components/Profile/About'));
-// const Expertise = React.lazy(() => import('@/components/Profile/Expertise'));
-// const Timeline = React.lazy(() => import('@/components/Profile/Timeline'));
-// const Project = React.lazy(() => import('@/components/Profile/Project'));
-
-// // 클라이언트 컴포넌트 동적 로딩
-// // import dynamic from 'next/dynamic';
-// // const Timeline = dynamic(() => import('./Timeline'), { ssr: false });
-
-// import '@/components/Profile/index.scss';
-// // import Loading from '@/app/loading';
-
-// const ProfileContent = () => (
-//     <FadeIn
-//         transitionDuration={700}
-//         className={clsx('fade-in-wrapper')}
-//         childClassName={clsx(['flex flex-col', 'min-h-screen'])}
-//     >
-//         <Main />
-//         <Expertise />
-//         <Timeline />
-//         <Project />
-//     </FadeIn>
-// );
-
-// const page = () => {
-//     return (
-//         <main className={clsx('main-container', ['relative', 'flex flex-col'])}>
-//             <Suspense fallback={<Loading />}>
-//                 <ProfileContent />
-//             </Suspense>
-//         </main>
-//     );
-// };
+const ProfileContent = () => (
+    <FadeIn
+        transitionDuration={700}
+        className={clsx('fade-in-wrapper')}
+        childClassName={clsx(['flex flex-col', 'min-h-screen'])}
+    >
+        <Main />
+        <Expertise />
+        <Timeline />
+        <Project />
+    </FadeIn>
+);
 
 const page = () => {
-    return <div>test</div>;
+    return (
+        <main className={clsx('main-container', ['relative', 'flex flex-col'])}>
+            <Suspense fallback={<Loading />}>
+                <ProfileContent />
+            </Suspense>
+        </main>
+    );
 };
 
 export default page;
